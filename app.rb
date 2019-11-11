82b0b47c00f06c84a19993220396a73433aad1ca
Sophia <382316736@qq.com>
2019-11-11 17:09:01 +0000

    done the farm index

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require ('sinatra')
 require ('sinatra/contrib/all')
 require_relative ('controllers/product_controller')
-# require_relative ('controllers/farm_controller')
+require_relative ('controllers/farm_controller')
 
 get '/' do
   erb (:index)
--- controllers/farm_controller.rb
+++ controllers/farm_controller.rb
@@ -1,1 +1,45 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+require_relative('../models/product.rb')
+require_relative('../models/farm.rb')
+also_reload('../models/*')
 
+get '/farms' do
+  @farms = Farm.all()
+  erb (:"farms/index")
+end
+
+# get '/farms/new' do
+#   @farms = Farm.all()
+#   erb (:"farms/new")
+# end
+#
+# get '/farms/:id' do
+#   @farm = Farm.find(params[:id])
+#   erb (:"farms/show")
+# end
+#
+# get '/farms/:id/edit' do
+#   @farm = Find.find(params[:id])
+#   erb (:"farms/edit")
+# end
+#
+# post '/farms' do
+#   @farm = Farm.new( params )
+#   @farm.save()
+#   erb( :"farms/create" )
+# end
+#
+# post '/farms/:id/delete' do
+#   @farm = Farm.find(params[:id])
+#   @farm.delete()
+#   erb( :"farms/delete" )
+# end
+#
+#
+# post '/farms/:id' do
+#   farm = Farm.new(params)
+#   farm.update
+#   redirect to "/farms/#{params['id']}"
+# end
